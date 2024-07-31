afb9309d858ea60868183bb4fcd32c46da0fe083
z h <zhanif02@gmail.com>
2024-07-31 16:28:00 +0100

    new videos section created

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import SideBar from "./SideBar";
+import { SideBar, Videos } from "./";
 
 const Feed = () => {
   return (
@@ -11,14 +11,27 @@ const Feed = () => {
           height: { sx: "auto", md: "92vh" },
           borderRight: "1px solid #3d3d3d",
           px: { sx: 0, md: 2 },
-        }}>
+        }}
+      >
         <SideBar />
         <Typography
           className="copyright"
           variant="body2"
-          sx={{ mt: 1.5, color: "#fff" }}>
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
           Copyright 2024 Yootube
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>Videos</span>
+        </Typography>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
